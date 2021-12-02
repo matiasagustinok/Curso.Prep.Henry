6d652259c8e359816426861ac15d770254bc86ec
matias carregno <kepp12311@gmail.com>
2021-12-02 18:44:05 -0300

    homework 1 progres

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = "Hola";
 const nuevoNum = 2;
 
 // Crea una variable booleana:
-const nuevoBool = var megusta = true;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -16,7 +16,7 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -28,8 +28,7 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
-  function devolverString("str")
-  return devolverString
+return devolverString
 
 }
 
@@ -37,9 +36,9 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  función Suma(x, y){
+  function Suma(x, y){
     return(x + y)
-    suma(5, 5)
+    suma (5, 5)
 
   }
 }
@@ -47,24 +46,28 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  function Resta(5, 4){
-    return( 5 - 4)
+  function Resta(x, y){
+    return( x - y)
+    resta (2, 1)
+    }
   }
-}
+
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  function multiplica(6, 2){
-    return(6 * 2)
+  function multiplica(x, y){
+    return(x * y)
+    multiplica (5, 5)
   }
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  function divide(4, 2){
-    return(4 / 2)
+  function divide(x, y){
+    return(x / y)
+    divide (4, 2)
   }
 }
 
@@ -72,15 +75,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  function sonIguales(5, 5){
-    if (5 === 5) {
-      return true
 
-    } else {
-      return false
-
-    }
-  }
 
 }
 
@@ -88,19 +83,18 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  function tienenMismaLongitud("hola" , "chau"){
-    if tienenMismaLongitud.length === {
-      return true
-
+  return tienenMismaLongitud.length
     }
-  }
-}
+  
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-
+if (num < 90)
+return true
+else 
+return false
 }
 
 function mayorQueCincuenta(num) {
